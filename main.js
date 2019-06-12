0119d5b31323c36aa54e9ed3628db2b82f15715e
Leonardo <perottoleonardos@gmail.com>
2019-06-12 14:05:36 -0300

    adiciona função para gerar lista

---
+++ main.js
@@ -0,0 +1,46 @@
+
+var input = document.querySelector('input');
+var btn = document.querySelector('button');
+var lista =  document.querySelector('table');
+
+var todos = [
+    'Fazer café',
+    'Passear com o cachorro',
+    'Estudar'
+]
+
+
+function geraLista(){
+
+    for(todo of todos){
+        var linhaTabela = document.createElement('tr');
+        var colunaTabela = document.createElement('td');
+        var colunaContent = document.createTextNode(todo);
+
+        var colunaLink = document.createElement('td');
+        var linkEditar = document.createElement('a');
+        var linkContent = document.createTextNode('Editar');
+        linkEditar.setAttribute('href', '#');
+
+        colunaLink.appendChild(linkEditar);
+        linkEditar.appendChild(linkContent);
+        
+
+        var linkDeletar = document.createElement('a');
+        var deletarContent = document.createTextNode(' Remover');
+        linkDeletar.setAttribute('href', '#');
+
+        colunaLink.appendChild(linkDeletar);
+        linkDeletar.appendChild(deletarContent);
+
+        colunaTabela.appendChild(colunaContent);
+        linhaTabela.appendChild(colunaTabela);
+        lista.appendChild(linhaTabela);
+        linhaTabela.appendChild(colunaLink);
+
+    }
+
+    input.value = '';
+}
+
+geraLista();
