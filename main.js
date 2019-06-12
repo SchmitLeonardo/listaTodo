774dc7737111c8f82b30bf42d0ad69547053b670
Leonardo <perottoleonardos@gmail.com>
2019-06-12 15:23:47 -0300

    Adiciona função editar todo

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var btn = document.querySelector('button');
 var lista =  document.getElementById('content');
 
 var todos = JSON.parse(localStorage.getItem('lista_todo')) || [];
-
 /*
 var todos = [
     'Fazer café',
@@ -24,10 +23,13 @@ function geraLista(){
         linhaTabela.appendChild(colunaTabela);
         lista.appendChild(linhaTabela);
 
+        var deletar = todos.indexOf(todo);
+
         var colunaLink = document.createElement('td');
         var linkEditar = document.createElement('a');
         var linkContent = document.createTextNode('Editar');
         linkEditar.setAttribute('href', '#');
+        linkEditar.setAttribute('onclick', 'editarTodo('+ deletar +')');
 
         colunaLink.appendChild(linkEditar);
         linkEditar.appendChild(linkContent);
@@ -51,18 +53,21 @@ function geraLista(){
         colunaRadio.appendChild(radioConcluido);
         linhaTabela.appendChild(colunaRadio);
     }
-
     input.value = '';
 }
 
 geraLista();
+
+btn.onclick = () => {
+    adicionarTodo();
+    salvarLocalStorage();
+}
 
 function limparTela(){
     lista.innerHTML = '';
 }
 
 function adicionarTodo(){
-
     limparTela();
     todos.push(input.value);
     geraLista();
@@ -70,16 +75,24 @@ function adicionarTodo(){
 
 function deletarTodo(posicao){
     todos.splice(posicao, 1);
+    console.log(posicao);
     limparTela();
     geraLista();
     salvarLocalStorage();
 }
 
-btn.onclick = () => {
-    adicionarTodo();
-    salvarLocalStorage();
-}
-
 function salvarLocalStorage(){
     localStorage.setItem('lista_todo', JSON.stringify(todos));
+}
+
+function editarTodo(posicao){
+    input.value = todos[posicao];
+    console.log(todos[posicao]);
+
+    btn.onclick = () => {
+        todos[posicao] = input.value;
+        limparTela();
+        geraLista();
+        salvarLocalStorage();
+    }
 }
