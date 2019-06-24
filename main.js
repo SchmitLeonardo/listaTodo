65da1615736d0c1c09707d6a608cdcd7314eac37
Leonardo <perottoleonardos@gmail.com>
2019-06-24 09:50:31 -0300

    Adicionada ações dos modals e lista

--- main.js
+++ main.js
@@ -1,125 +1,117 @@
+var modal = document.querySelector('.modal-container');
+var fechaModal = document.querySelector('.cancelar');
+var todo = document.querySelector('.todo');
+var todosContent = document.querySelector('.todos');
+var todoSegundo = document.querySelector('.todo:nth-of-type(1n+2)');
+var opcoes = document.querySelector('.opcoes');
 
-var input = document.querySelector('input');
-var btn = document.querySelector('button');
-var lista =  document.getElementById('content');
+var principal = { 
+    lista: JSON.parse(localStorage.getItem('lista_todos')) || [],
+    geraTodo: () =>{
+      for(index in principal.lista){
+        var item = principal.lista[index];
+        var novoTodo = document.createElement('div');
+        novoTodo.innerHTML = `<div class="todo">
+            <input type="checkbox" name="checkbox" class="checkbox">
+            <h3 class="tituloTodo" onclick="principal.mostrarOpcoes(`+index+`)">`+item+`</h3>
+        </div>
+        <div class="opcoes">
+            <h3 class="editar-todo" onclick="principal.modalEditar(`+index+`)"><img src="img/edit.svg"> Editar</h3>
+            <h3 class="deletar-todo" onclick="principal.modalExcluir(`+index+`)"><img src="img/delete.svg"> Remover</h3>
+        </div>`
 
-var todos = JSON.parse(localStorage.getItem('lista_todo')) || [];
-var checks = JSON.parse(localStorage.getItem('lista_checks')) || [];
+        todosContent.appendChild(novoTodo);
+      }
 
-function geraLista(){
-    for(index in todos){
-        var todo = todos[index];
-        var linhaTabela = document.createElement('tr');
-        var colunaTabela = document.createElement('td');
-        var colunaContent = document.createTextNode(todo);
-
-        colunaTabela.appendChild(colunaContent);
-        linhaTabela.appendChild(colunaTabela);
-        lista.appendChild(linhaTabela);
-
-        var posicao = todos.indexOf(todo);
-        var posicaoCheck = checks.indexOf(checks[index]);
-
-        var colunaLink = document.createElement('td');
-        var linkEditar = document.createElement('a');
-        var linkContent = document.createTextNode('Editar');
-        linkEditar.setAttribute('href', '#');
-        linkEditar.setAttribute('onclick', 'editarTodo('+ posicao +')');
-
-        colunaLink.appendChild(linkEditar);
-        linkEditar.appendChild(linkContent);
-        linhaTabela.appendChild(colunaLink);            
-
-        var linkDeletar = document.createElement('a');
-        var deletarContent = document.createTextNode(' Remover');
-        linkDeletar.setAttribute('href', '#');
-        linkDeletar.setAttribute('onclick', 'deletarTodo('+ posicao+')');
-
-        colunaLink.appendChild(linkDeletar);
-        linkDeletar.appendChild(deletarContent);
-
-        var colunaCheck = document.createElement('td');
-        var checkConcluido = document.createElement('input');
-        checkConcluido.setAttribute('type', 'checkbox');
-        checkConcluido.setAttribute('onchange', 'checkList('+index+')');
-        if(checks[index] === true){
-            checkConcluido.setAttribute('checked', checks[index]);
-        }
-
-        colunaCheck.appendChild(checkConcluido);
-        linhaTabela.appendChild(colunaCheck);
+      var botaoAdicionar = document.querySelector('.botao-adicionar');
+      botaoAdicionar.setAttribute('onclick', 'principal.modalAdicionar()');
+    
+      // var botaoAdicionar = document.querySelector('.botao-adicionar');
+      // botaoAdicionar.setAttribute('onclick', 'principal.modalAdicionar()');
+      
+      window.onclick = function(event){
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+      }
+    },
+    limparTela: () => {
+      todosContent.innerHTML = '';
+    },
+    salvarLocalStorage: () => {
+      localStorage.setItem('lista_todos', JSON.stringify(principal.lista));
+    },
+    mostrarOpcoes: (pos) => {
+      var opcoes = document.querySelectorAll('.opcoes');
+      if(opcoes[pos].style.display == "none"){
+        opcoes[pos].style.display = "flex";
+        opcoes[pos].style.animation = "opcoes .8s forwards";
+      }else{
+        opcoes[pos].style.display = "none";
+        opcoes[pos].style.animation = "opcoes .8s backwards";
+      }
+    },
+    modalAdicionar: () => {
+        modal.style.display = "flex";
+        modal.innerHTML = `<div class="modal">
+        <div class="modal-header">
+            <h3>Adicionar Todo</h3>
+        </div>
+        <div class="modal-body">
+            <form>
+                <input type="text" class="adiciona-todo" placeholder="Adicionar Todo"/>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <a href="#" onclick="principal.adicionaTodo()"><p>Adicionar</p></a>
+            <a href="#" class="cancelar" onclick="principal.fecharModal()"><p>Cancelar</p></a>
+        </div>
+      </div>`
+    },
+    modalEditar: (pos) => {
+      // console.log(this.document.querySelectorAll('.tituloTodo').textContent);
+      var content = principal.lista[pos];
+      modal.style.display = "flex";
+      modal.innerHTML = `<div class="modal">
+            <div class="modal-header">
+                <h3>Editar Todo</h3>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <input type="text" class="todoEditado" value="`+content+`">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#" onclick="principal.editarTodo(`+pos+`)"><p>Adicionar</p></a>
+                <a href="#" class="cancelar" onclick="principal.fecharModal()"><p>Cancelar</p></a>
+            </div>
+        </div>`;
+    },
+    fecharModal: () => {
+      modal.style.display = "none";
+    },
+    editarTodo: (pos) => {
+      principal.lista[pos] = this.document.querySelector('.todoEditado').value;
+      principal.limparTela();
+      principal.geraTodo();
+      principal.fecharModal();
+      principal.salvarLocalStorage();
+    },
+    modalExcluir: (pos) => {
+      principal.lista.splice(pos, 1);
+      principal.limparTela();
+      principal.geraTodo();
+      principal.salvarLocalStorage();
+    },
+    adicionaTodo: () => {
+      var novoTodo = this.document.querySelector('.adiciona-todo').value;
+      principal.lista.push(novoTodo);
+      principal.limparTela();
+      principal.geraTodo();
+      principal.fecharModal();
+      principal.salvarLocalStorage();
     }
-    input.value = '';
 }
 
-geraLista();
+principal.geraTodo();
 
-btn.onclick = () => {
-    adicionarTodo();
-    salvarLocalStorage();
-}
-
-function limparTela(){
-    lista.innerHTML = '';
-}
-
-function adicionarTodo(){
-    limparTela();
-    todos.push(input.value);
-    adicionarCheck();
-    geraLista();
-}
-
-function deletarTodo(posicao){
-    todos.splice(posicao, 1);
-    deletarCheck();
-    limparTela();
-    geraLista();
-    salvarLocalStorage();
-}
-
-function salvarLocalStorage(){
-    localStorage.setItem('lista_todo', JSON.stringify(todos));
-}
-
-function editarTodo(posicao){
-    input.value = todos[posicao];
-    console.log(todos[posicao]);
-
-    btn.onclick = () => {
-        todos[posicao] = input.value;
-        limparTela();
-        geraLista();
-        salvarLocalStorage();
-    }
-}
-
-function salvarLocalStorageCheck(){
-    localStorage.setItem('lista_checks', JSON.stringify(checks));
-}
-
-function checkList(check){
-    console.log(check);
-    if(checks[check] === true || checks[check] === false){
-        if(checks[check] === true){
-            checks[check] = false;
-        }else{
-            checks[check] = true;
-        }
-        salvarLocalStorageCheck();
-    }else{
-        adicionarCheck(check);
-    }
-}
-
-function adicionarCheck(check){
-    checks.push(checks[check] = false);
-    salvarLocalStorageCheck();
-}
-
-function deletarCheck(){
-    checks.splice(checks[this], 1);
-    salvarLocalStorageCheck();
-    limparTela();
-    geraLista();
-}
